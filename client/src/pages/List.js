b8534ba2fbec7373a01cc92e8d16c93df968f039
MissDine <enidnyatichi1212@gmail.com>
2022-08-08 09:33:49 +0300

    Feature edit

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import "../components/Home.css";
+import React, { useState, useEffect } from "react";
+import "./Home.css";
 
 
 function List() {
@@ -10,12 +10,10 @@ function List() {
         list_id:null
     });
     const [tasklist, setTaskList] = useState([]);
-    const fetchData = () => {
-        fetch("/lists").then(response => response.json()).then(data =>setTaskList(data))
-    }
-    if(tasklist.length === 0) {
-        fetchData()
-    }
+    useEffect(() => {
+        fetch("https://todo-sinatra-app.herokuapp.com/").then(response => response.json()).then(data =>setTaskList(data))
+
+    },[])
     function handleEdit(id, name,list_id){
         setTask({
             ...task,
@@ -23,11 +21,11 @@ function List() {
             name:name,
             list_id:list_id
         }) 
-        fetchData() 
+        
     }
 
     function handleDelete(id){
-        fetch(`/tasks/${id}`,{
+        fetch(`https://todo-sinatra-app.herokuapp.com/tasks/${id}`,{
             method: "DELETE"
         })
         const remaining = tasklist.filter((item)=>item.id!== id)
@@ -35,7 +33,8 @@ function List() {
     }
     function handleSubmit(e){
         e.preventDefault()
-        fetch(`/tasks/${task.user_id}`,{
+        console.log(task);
+        fetch(`https://todo-sinatra-app.herokuapp.com/tasks/${task.user_id}`,{
             method: "PATCH",
             headers:{
                 "Content-Type":"application/json"
@@ -57,43 +56,40 @@ function List() {
     
     return(
         <>
-        <div>
-        <h1 className="text-black text-center py-10">List of all tasks:</h1>
-        <form onSubmit = {handleSubmit} className='flex pb-10 justify-center items-center'>
-            <input
-            type="text"
-            name="text"
-            id="text"
-            className="mr-4 appearance-none rounded-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            value = {task.name}
-            onChange={(e) => setTask({
-                ...task,
-                name: e.target.value
-            })}
-            placeholder="Edit task in the list..."
-            />
+            <div>
+            <h1 className="lii">List of all tasks:</h1>
+            <form onSubmit = {handleSubmit}>
+                <input
+                type="text"
+                name="text"
+                id="text"
+                value = {task.name}
+                onChange={(e) => setTask({
+                    ...task,
+                    name: e.target.value
+                })}
+                placeholder="Edit task in the list..."
+                />
 
-            <input className = "sub"
-            type="submit" value="Edit list.." />
-        </form>
-        <ol>
-        {tasklist !== [] ? tasklist.map(item => (
-        <div key={item.id} className='pb-6'>
-            <li className="flex justify-around text-lg py-2" key = {item.id}>
-            <p className="text-xl font-bold">{item.name}</p> 
-            </li>
-            <ul>
-                {item.tasks.length !== 0 ? item.tasks.map((task) => (
-                <li className="text-black my-4 flex justify-center" key = {task.id}>{task.name}
-                <button className=" bg-green-600 rounded py-2 px-4 ml-8"
-                onClick = {()=>handleEdit(task.id, task.name,item.id)} >Edit</button> 
-                <button className="bg-red-600 rounded py-2 px-4 ml-8"
-                onClick={()=>handleDelete(task.id)}>Delete</button></li>)) : "No data"}
-            </ul>
-        </div>
-        )) : <div>Task list is empty</div>}
+                <input className = "sub"
+                type="submit" value="Edit list.." />
+            </form>
+            <ol>
+            {tasklist.map(item => (
+            <div>
+                <li className="list" key = {item.id}>
+                {item.name} 
+                </li>
+                <ul>
+                    {item.tasks.map(task=>(<li className="taskl" key = {task.id}>{task.name}
+                    <button className="edit"
+                onClick = {()=>handleEdit(task.id, task.name,item.id)} >Edit</button> <button className="dele"
+                onClick={()=>handleDelete(task.id)}>Delete</button></li>))}
+                </ul>
+            </div>
+            ))}
 
-        </ol>
+            </ol>
         </div>
         </>
         
